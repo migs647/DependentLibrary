b3a53aba53a7cd06f1a2d89a47a0eac31bc5b94e
Cody Garvin <emig647@gmail.com>
2020-06-24 16:41:27 -0700

    Added Swift Package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "DependentLibrary",
+    platforms: [
+        .iOS(.v12), .tvOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -14,15 +17,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            name: "Analytics",
+            url: "git@github.com:migs647/analytics-ios.git",
+            .branch("trash2")
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DependentLibrary",
-            dependencies: []),
-        .testTarget(
-            name: "DependentLibraryTests",
-            dependencies: ["DependentLibrary"]),
+            dependencies: ["Analytics"]),
     ]
 )
--- Sources/DependentLibrary/DependentLibrary.swift
+++ Sources/DependentLibrary/DependentLibrary.swift
@@ -1,3 +1,13 @@
+import Analytics
+
 struct DependentLibrary {
-    var text = "Hello, World!"
+    var writeKey = "8XpdAWa7qJVBJMK8V4FfXQOrnvCzu3Ie"
+    func startAnalytics() {
+        let configuration = AnalyticsConfiguration(writeKey: "8XpdAWa7qJVBJMK8V4FfXQOrnvCzu3Ie")
+        // Enable this to record certain application events automatically!
+        configuration.trackApplicationLifecycleEvents = true
+        // Enable this to record screen views automatically!
+        configuration.recordScreenViews = true
+        Analytics.setup(with: configuration)
+    }
 }
